bcc0b1a340d2700f8247dc998e61b58d2c128043
Gabriel Angelo Gerardino <gabriel_angelo_gerardino@dlsu.edu.ph>
2024-01-24 16:07:56 +0800

    implemented #1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 #define LIMIT 10000000
+#define DEFAULT_THREADS 1
 
 /*
 This function checks if an integer n is prime.
@@ -14,24 +15,36 @@ Returns true if n is prime, and false otherwise.
 bool check_prime(const int &n);
 
 int main() {
-  std::vector<int> primes;
+    int upper_bound = 0;
+    int num_threads = DEFAULT_THREADS; // default
     
-  for (int current_num = 2; current_num <= LIMIT; current_num++) {
-    if (check_prime(current_num)) {
-      primes.push_back(current_num);
+    std::<cout << "Enter upper bound of integers";
+    std::cin >> upper_bound;
+    
+    std::<cout << "Enter the number of threads";
+    std:: cin >> num_threads;
+    if (num_threads <= 0) // error checking 
+       num_threads = DEFAULT_THREADS; 
+    
+    std::vector<int> primes;
+        
+    for (int current_num = 2; current_num <= upper_bound; current_num++) {
+        
+        if (check_prime(current_num)) {
+           primes.push_back(current_num);
+        }
     }
-  }
-
-  std::cout << primes.size() << " primes were found." << std::endl;
-
-  return 0;
+    
+    std::cout << primes.size() << " primes were found." << std::endl;
+    
+    return 0;
 }
 
 bool check_prime(const int &n) {
-  for (int i = 2; i * i <= n; i++) {
-    if (n % i == 0) {
-      return false;
-    }
-  }
-  return true;
+     for (int i = 2; i * i <= n; i++) {
+         if (n % i == 0) {
+            return false;
+         }
+     }
+   return true; 
 }
